166b86ff8ad8a2446087c2c3b41cf3be592b2b51
shira goldstein <shiragoldstein2867@gmail.com>
2025-10-21 09:10:06 +0300

    header

---
+++ src/app/Components/Header/Header.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './Header.module.css';
+
+
+export default function Header() {
+
+  return (
+    <header className={styles.header}>
+      <h1 className={styles.title}>My Store</h1>
+    </header>
+  );
+}
